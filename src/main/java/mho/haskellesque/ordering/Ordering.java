a799f1b4e40f9074532f791536d705eebca170b5
mhogrefe <mikhailhogrefe@gmail.com>
2014-10-02 22:22:14 -0400

    added Comparator-based compares, some annotations

--- src/main/java/mho/haskellesque/ordering/Ordering.java
+++ src/main/java/mho/haskellesque/ordering/Ordering.java
@@ -1,9 +1,14 @@
 package mho.haskellesque.ordering;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Comparator;
+
 public enum Ordering {
     LT, EQ, GT;
 
-    public Ordering reverse() {
+    public @NotNull Ordering invert() {
         switch (this) {
             case LT: return GT;
             case EQ: return EQ;
@@ -12,11 +17,59 @@ public enum Ordering {
         return null;
     }
 
-    public static <T extends Comparable<T>> Ordering compare(T a, T b) {
-        int c = a.compareTo(b);
-        if (c < 0) return LT;
-        if (c > 0) return GT;
+    public static Ordering fromInt(int i) {
+        if (i > 0) return GT;
+        if (i < 0) return LT;
         return EQ;
+    }
+
+    public int toInt() {
+        switch (this) {
+            case LT: return -1;
+            case EQ: return 0;
+            case GT: return 1;
+        }
+        return 0; //doesn't happen
+    }
+
+    public static @NotNull <T extends Comparable<T>> Ordering compare(@NotNull T a, @NotNull T b) {
+        return fromInt(a.compareTo(b));
+    }
+
+    public static @NotNull <T extends Comparable<T>> Ordering compare(
+            @NotNull Comparator<T> comparator,
+            @NotNull T a,
+            @NotNull T b
+    ) {
+        return fromInt(comparator.compare(a, b));
+    }
+
+    public static @NotNull <T extends Comparable<T>> Ordering compareNullable(@Nullable T a, @Nullable T b) {
+        if (a == null && b == null) {
+            return EQ;
+        } else if (a == null) {
+            return LT;
+        } else if (b == null) {
+            return GT;
+        } else {
+            return compare(a, b);
+        }
+    }
+
+    public static @NotNull <T extends Comparable<T>> Ordering compareNullable(
+            Comparator<T> comparator,
+            @Nullable T a,
+            @Nullable T b
+    ) {
+        if (a == null && b == null) {
+            return EQ;
+        } else if (a == null) {
+            return LT;
+        } else if (b == null) {
+            return GT;
+        } else {
+            return compare(comparator, a, b);
+        }
     }
 
     public static <T extends Comparable<T>> boolean eq(T a, T b) {
@@ -24,7 +77,7 @@ public enum Ordering {
     }
 
     public static <T extends Comparable<T>> boolean equalTo(T a, T b) {
-        return a.compareTo(b) == 0;
+        return eq(a, b);
     }
 
     public static <T extends Comparable<T>> boolean ne(T a, T b) {
@@ -32,7 +85,7 @@ public enum Ordering {
     }
 
     public static <T extends Comparable<T>> boolean notEqualTo(T a, T b) {
-        return a.compareTo(b) != 0;
+        return ne(a, b);
     }
 
     public static <T extends Comparable<T>> boolean lt(T a, T b) {
@@ -40,7 +93,7 @@ public enum Ordering {
     }
 
     public static <T extends Comparable<T>> boolean lessThan(T a, T b) {
-        return a.compareTo(b) < 0;
+        return lt(a, b);
     }
 
     public static <T extends Comparable<T>> boolean gt(T a, T b) {
@@ -48,7 +101,7 @@ public enum Ordering {
     }
 
     public static <T extends Comparable<T>> boolean greaterThan(T a, T b) {
-        return a.compareTo(b) > 0;
+        return gt(a, b);
     }
 
     public static <T extends Comparable<T>> boolean le(T a, T b) {
@@ -56,7 +109,7 @@ public enum Ordering {
     }
 
     public static <T extends Comparable<T>> boolean lessThanOrEqualTo(T a, T b) {
-        return a.compareTo(b) <= 0;
+        return le(a, b);
     }
 
     public static <T extends Comparable<T>> boolean ge(T a, T b) {
@@ -64,6 +117,54 @@ public enum Ordering {
     }
 
     public static <T extends Comparable<T>> boolean greaterThanOrEqualTo(T a, T b) {
-        return a.compareTo(b) >= 0;
+        return ge(a, b);
+    }
+
+    public static <T extends Comparable<T>> boolean eq(Comparator<T> comparator, T a, T b) {
+        return comparator.compare(a, b) == 0;
+    }
+
+    public static <T extends Comparable<T>> boolean equalTo(Comparator<T> comparator, T a, T b) {
+        return eq(comparator, a, b);
+    }
+
+    public static <T extends Comparable<T>> boolean ne(Comparator<T> comparator, T a, T b) {
+        return comparator.compare(a, b) != 0;
+    }
+
+    public static <T extends Comparable<T>> boolean notEqualTo(Comparator<T> comparator, T a, T b) {
+        return ne(comparator, a, b);
+    }
+
+    public static <T extends Comparable<T>> boolean lt(Comparator<T> comparator, T a, T b) {
+        return comparator.compare(a, b) < 0;
+    }
+
+    public static <T extends Comparable<T>> boolean lessThan(Comparator<T> comparator, T a, T b) {
+        return lt(comparator, a, b);
+    }
+
+    public static <T extends Comparable<T>> boolean gt(Comparator<T> comparator, T a, T b) {
+        return comparator.compare(a, b) > 0;
+    }
+
+    public static <T extends Comparable<T>> boolean greaterThan(Comparator<T> comparator, T a, T b) {
+        return gt(comparator, a, b);
+    }
+
+    public static <T extends Comparable<T>> boolean le(Comparator<T> comparator, T a, T b) {
+        return comparator.compare(a, b) <= 0;
+    }
+
+    public static <T extends Comparable<T>> boolean lessThanOrEqualTo(Comparator<T> comparator, T a, T b) {
+        return le(comparator, a, b);
+    }
+
+    public static <T extends Comparable<T>> boolean ge(Comparator<T> comparator, T a, T b) {
+        return comparator.compare(a, b) >= 0;
+    }
+
+    public static <T extends Comparable<T>> boolean greaterThanOrEqualTo(Comparator<T> comparator, T a, T b) {
+        return ge(comparator, a, b);
     }
 }
