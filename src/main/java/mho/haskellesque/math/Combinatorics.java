3016a066ae0a964201d59a43c318a7574916f479
mhogrefe <mikhailhogrefe@gmail.com>
2014-10-06 19:51:30 -0400

    added some tuple generators

--- src/main/java/mho/haskellesque/math/Combinatorics.java
+++ src/main/java/mho/haskellesque/math/Combinatorics.java
@@ -1,6 +1,14 @@
 package mho.haskellesque.math;
 
+import mho.haskellesque.iterables.Exhaustive;
+import mho.haskellesque.iterables.IndexedIterable;
+import mho.haskellesque.tuples.Pair;
+import mho.haskellesque.tuples.Quadruple;
+import mho.haskellesque.tuples.Triple;
+
 import java.math.BigInteger;
+import java.util.Optional;
+import java.util.function.Function;
 
 import static mho.haskellesque.iterables.IterableUtils.*;
 import static mho.haskellesque.ordering.Ordering.*;
@@ -38,5 +46,78 @@ public class Combinatorics {
             b = c;
         }
         return c;
+    }
+
+    public static <S, T> Iterable<Pair<S, T>> cartesianPairs(Iterable<S> fsts, Iterable<T> snds) {
+        return concatMap(p -> zip(repeat(p.fst), p.snd), zip(fsts, repeat(snds)));
+    }
+
+    public static <A, B, C> Iterable<Triple<A, B, C>> cartesianTriples(
+            Iterable<A> as,
+            Iterable<B> bs,
+            Iterable<C> cs
+    ) {
+        return map(
+                p -> new Triple<>(p.fst, p.snd.fst, p.snd.snd),
+                cartesianPairs(as, (Iterable<Pair<B, C>>) cartesianPairs(bs, cs))
+        );
+    }
+
+    public static <A, B, C, D> Iterable<Quadruple<A, B, C, D>> cartesianQuadruples(
+            Iterable<A> as,
+            Iterable<B> bs,
+            Iterable<C> cs,
+            Iterable<D> ds
+    ) {
+        return map(
+                p -> new Quadruple<>(p.fst.fst, p.fst.snd, p.snd.fst, p.snd.snd),
+                cartesianPairs(
+                        (Iterable<Pair<A, B>>)cartesianPairs(as, bs),
+                        (Iterable<Pair<C, D>>) cartesianPairs(cs, ds)
+                )
+        );
+    }
+
+    public static <S, T> Iterable<Pair<S, T>> exponentialPairs(Iterable<S> fsts, Iterable<T> snds) {
+        IndexedIterable<S> fstii = new IndexedIterable<>(fsts);
+        IndexedIterable<T> sndii = new IndexedIterable<>(snds);
+        Function<BigInteger, Optional<Pair<S, T>>> f = bi -> {
+            Pair<BigInteger, BigInteger> p = BasicMath.exponentialDemux(bi);
+            assert p.fst != null;
+            Optional<S> optFst = fstii.get(p.fst.intValue());
+            if (!optFst.isPresent()) return Optional.empty();
+            assert p.snd != null;
+            Optional<T> optSnd = sndii.get(p.snd.intValue());
+            if (!optSnd.isPresent()) return Optional.empty();
+            return Optional.of(new Pair<S, T>(optFst.get(), optSnd.get()));
+        };
+        return map(
+                Optional::get,
+                filter(
+                        Optional<Pair<S, T>>::isPresent,
+                        (Iterable<Optional<Pair<S, T>>>) map(bi -> f.apply(bi), Exhaustive.BIG_INTEGERS)
+                )
+        );
+    }
+
+    public static <T> Iterable<Pair<T, T>> exponentialPairs(Iterable<T> xs) {
+        IndexedIterable<T> ii = new IndexedIterable<>(xs);
+        Function<BigInteger, Optional<Pair<T, T>>> f = bi -> {
+            Pair<BigInteger, BigInteger> p = BasicMath.exponentialDemux(bi);
+            assert p.fst != null;
+            Optional<T> optFst = ii.get(p.fst.intValue());
+            if (!optFst.isPresent()) return Optional.empty();
+            assert p.snd != null;
+            Optional<T> optSnd = ii.get(p.snd.intValue());
+            if (!optSnd.isPresent()) return Optional.empty();
+            return Optional.of(new Pair<T, T>(optFst.get(), optSnd.get()));
+        };
+        return map(
+                Optional::get,
+                filter(
+                        Optional<Pair<T, T>>::isPresent,
+                        (Iterable<Optional<Pair<T, T>>>) map(bi -> f.apply(bi), Exhaustive.NATURAL_BIG_INTEGERS)
+                )
+        );
     }
 }
