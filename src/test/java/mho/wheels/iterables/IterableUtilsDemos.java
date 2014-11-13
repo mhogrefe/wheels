d45d36b02a2d3d18997cf00f9e02142aad4b8b4e
mhogrefe <mikhailhogrefe@gmail.com>
2014-11-12 20:44:04 -0500

    added toString and fromString demos

--- src/test/java/mho/wheels/iterables/IterableUtilsDemos.java
+++ src/test/java/mho/wheels/iterables/IterableUtilsDemos.java
@@ -9,14 +9,10 @@ import java.util.Random;
 import static mho.wheels.iterables.IterableUtils.*;
 
 public class IterableUtilsDemos {
-    private static final boolean USE_RANDOM = true;
+    private static final boolean USE_RANDOM = false;
     private static int LIMIT;
 
     private static IterableProvider P;
-
-    public static void main(String[] args) {
-        demoToList_String();
-    }
 
     private static void initialize() {
         if (USE_RANDOM) {
@@ -65,6 +61,21 @@ public class IterableUtilsDemos {
         initialize();
         for (String s : take(LIMIT, P.strings())) {
             System.out.println("toList(" + s + ") = " + toList(s));
+        }
+    }
+
+    public static void demoToString() {
+        initialize();
+        for (List<Integer> list : take(LIMIT, P.lists(P.withNull(P.integers())))) {
+            String listString = tail(init(list.toString()));
+            System.out.println("toString(" + listString + ") = " + IterableUtils.toString(list));
+        }
+    }
+
+    public static void demoFromString() {
+        initialize();
+        for (String s : take(LIMIT, P.strings())) {
+            System.out.println("fromString(" + s + ") = " + toList(fromString(s)));
         }
     }
 }
