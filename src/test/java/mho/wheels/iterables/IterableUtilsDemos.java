abbcc63c6e3f4376ba543ab1bfd6053738732ff7
mhogrefe <mikhailhogrefe@gmail.com>
2014-11-12 20:39:48 -0500

    added toList demos

--- src/test/java/mho/wheels/iterables/IterableUtilsDemos.java
+++ src/test/java/mho/wheels/iterables/IterableUtilsDemos.java
@@ -9,10 +9,14 @@ import java.util.Random;
 import static mho.wheels.iterables.IterableUtils.*;
 
 public class IterableUtilsDemos {
-    private static final boolean USE_RANDOM = false;
+    private static final boolean USE_RANDOM = true;
     private static int LIMIT;
 
     private static IterableProvider P;
+
+    public static void main(String[] args) {
+        demoToList_String();
+    }
 
     private static void initialize() {
         if (USE_RANDOM) {
@@ -46,6 +50,21 @@ public class IterableUtilsDemos {
             list.addAll(p.b);
             addTo(p.a, list);
             System.out.println("addTo(" + p.a + ", " + p.b + ") => " + list);
+        }
+    }
+
+    public static void demoToList_Iterable() {
+        initialize();
+        for (List<Integer> list : take(LIMIT, P.lists(P.withNull(P.integers())))) {
+            String listString = tail(init(list.toString()));
+            System.out.println("toList(" + listString + ") = " + toList(list));
+        }
+    }
+
+    public static void demoToList_String() {
+        initialize();
+        for (String s : take(LIMIT, P.strings())) {
+            System.out.println("toList(" + s + ") = " + toList(s));
         }
     }
 }
