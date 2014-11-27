3969331b8b36e6986e7412ea138c3dead6061e2e
mhogrefe <mikhailhogrefe@gmail.com>
2014-11-26 20:13:08 -0500

    added remaining integral read and findIn tests

--- src/test/java/mho/wheels/misc/ReadersTest.java
+++ src/test/java/mho/wheels/misc/ReadersTest.java
@@ -2,6 +2,8 @@ package mho.wheels.misc;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 import static mho.wheels.misc.Readers.*;
 import static org.junit.Assert.*;
 
@@ -133,6 +135,123 @@ public class ReadersTest {
         assertFalse(findByteIn("").isPresent());
         assertFalse(findByteIn("hello").isPresent());
         assertFalse(findByteIn("vdfsvfbf").isPresent());
+    }
+
+    @Test
+     public void testReadShort() {
+        aeq(readShort("0").get(), "0");
+        aeq(readShort("5").get(), "5");
+        aeq(readShort("-100").get(), "-100");
+        aeq(readShort(Integer.toString(Short.MAX_VALUE)).get(), "32767");
+        aeq(readShort(Integer.toString(Short.MIN_VALUE)).get(), "-32768");
+        assertFalse(readShort(Integer.toString(Short.MAX_VALUE + 1)).isPresent());
+        assertFalse(readShort(Integer.toString(Short.MIN_VALUE - 1)).isPresent());
+        assertFalse(readShort(" 1").isPresent());
+        assertFalse(readShort("00").isPresent());
+        assertFalse(readShort("-0").isPresent());
+        assertFalse(readShort("0xff").isPresent());
+        assertFalse(readShort("0xff").isPresent());
+        assertFalse(readShort("2 ").isPresent());
+        assertFalse(readShort("--1").isPresent());
+        assertFalse(readShort("1-2").isPresent());
+        assertFalse(readShort("+4").isPresent());
+    }
+
+    @Test
+    public void testFindShortIn() {
+        aeq(findShortIn("abcd1234xyz").get(), "(1234, 4)");
+        aeq(findShortIn("abcd8234xyz").get(), "(8234, 4)");
+        aeq(findShortIn("0123").get(), "(0, 0)");
+        aeq(findShortIn("a-23").get(), "(-23, 1)");
+        aeq(findShortIn("---34--4").get(), "(-34, 2)");
+        aeq(findShortIn(" 20.1 ").get(), "(20, 1)");
+        aeq(findShortIn("abcd" + Short.MAX_VALUE + "xyz").get(), "(32767, 4)");
+        aeq(findShortIn("abcd" + Short.MIN_VALUE + "xyz").get(), "(-32768, 4)");
+        aeq(findShortIn("abcd" + (Short.MAX_VALUE + 1) + "xyz").get(), "(3276, 4)");
+        aeq(findShortIn("abcd" + (Short.MIN_VALUE - 1) + "xyz").get(), "(-3276, 4)");
+        assertFalse(findShortIn("").isPresent());
+        assertFalse(findShortIn("hello").isPresent());
+        assertFalse(findShortIn("vdfsvfbf").isPresent());
+    }
+
+    @Test
+     public void testReadInteger() {
+        aeq(readInteger("0").get(), "0");
+        aeq(readInteger("5").get(), "5");
+        aeq(readInteger("-100").get(), "-100");
+        aeq(readInteger(Integer.toString(Integer.MAX_VALUE)).get(), "2147483647");
+        aeq(readInteger(Integer.toString(Integer.MIN_VALUE)).get(), "-2147483648");
+        assertFalse(readInteger(Long.toString((long) Integer.MAX_VALUE + 1)).isPresent());
+        assertFalse(readInteger(Long.toString((long) Integer.MIN_VALUE - 1)).isPresent());
+        assertFalse(readInteger(" 1").isPresent());
+        assertFalse(readInteger("00").isPresent());
+        assertFalse(readInteger("-0").isPresent());
+        assertFalse(readInteger("0xff").isPresent());
+        assertFalse(readInteger("0xff").isPresent());
+        assertFalse(readInteger("2 ").isPresent());
+        assertFalse(readInteger("--1").isPresent());
+        assertFalse(readInteger("1-2").isPresent());
+        assertFalse(readInteger("+4").isPresent());
+    }
+
+    @Test
+    public void testFindIntegerIn() {
+        aeq(findIntegerIn("abcd1234xyz").get(), "(1234, 4)");
+        aeq(findIntegerIn("abcd8234xyz").get(), "(8234, 4)");
+        aeq(findIntegerIn("0123").get(), "(0, 0)");
+        aeq(findIntegerIn("a-23").get(), "(-23, 1)");
+        aeq(findIntegerIn("---34--4").get(), "(-34, 2)");
+        aeq(findIntegerIn(" 20.1 ").get(), "(20, 1)");
+        aeq(findIntegerIn("abcd" + Integer.MAX_VALUE + "xyz").get(), "(2147483647, 4)");
+        aeq(findIntegerIn("abcd" + Integer.MIN_VALUE + "xyz").get(), "(-2147483648, 4)");
+        aeq(findIntegerIn("abcd" + ((long) Integer.MAX_VALUE + 1) + "xyz").get(), "(214748364, 4)");
+        aeq(findIntegerIn("abcd" + ((long) Integer.MIN_VALUE - 1) + "xyz").get(), "(-214748364, 4)");
+        assertFalse(findIntegerIn("").isPresent());
+        assertFalse(findIntegerIn("hello").isPresent());
+        assertFalse(findIntegerIn("vdfsvfbf").isPresent());
+    }
+
+    @Test
+    public void testReadLong() {
+        aeq(readLong("0").get(), "0");
+        aeq(readLong("5").get(), "5");
+        aeq(readLong("-100").get(), "-100");
+        aeq(readLong(Long.toString(Long.MAX_VALUE)).get(), "9223372036854775807");
+        aeq(readLong(Long.toString(Long.MIN_VALUE)).get(), "-9223372036854775808");
+        assertFalse(readLong(BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE).toString()).isPresent());
+        assertFalse(readLong(BigInteger.valueOf(Long.MIN_VALUE).subtract(BigInteger.ONE).toString()).isPresent());
+        assertFalse(readLong(" 1").isPresent());
+        assertFalse(readLong("00").isPresent());
+        assertFalse(readLong("-0").isPresent());
+        assertFalse(readLong("0xff").isPresent());
+        assertFalse(readLong("0xff").isPresent());
+        assertFalse(readLong("2 ").isPresent());
+        assertFalse(readLong("--1").isPresent());
+        assertFalse(readLong("1-2").isPresent());
+        assertFalse(readLong("+4").isPresent());
+    }
+
+    @Test
+    public void testFindLongIn() {
+        aeq(findLongIn("abcd1234xyz").get(), "(1234, 4)");
+        aeq(findLongIn("abcd8234xyz").get(), "(8234, 4)");
+        aeq(findLongIn("0123").get(), "(0, 0)");
+        aeq(findLongIn("a-23").get(), "(-23, 1)");
+        aeq(findLongIn("---34--4").get(), "(-34, 2)");
+        aeq(findLongIn(" 20.1 ").get(), "(20, 1)");
+        aeq(findLongIn("abcd" + Long.MAX_VALUE + "xyz").get(), "(9223372036854775807, 4)");
+        aeq(findLongIn("abcd" + Long.MIN_VALUE + "xyz").get(), "(-9223372036854775808, 4)");
+        aeq(
+                findLongIn("abcd" + BigInteger.valueOf(Integer.MAX_VALUE).add(BigInteger.ONE) + "xyz").get(),
+                "(2147483648, 4)"
+        );
+        aeq(
+                findLongIn("abcd" + BigInteger.valueOf(Integer.MIN_VALUE).subtract(BigInteger.ONE) + "xyz").get(),
+                "(-2147483649, 4)"
+        );
+        assertFalse(findLongIn("").isPresent());
+        assertFalse(findLongIn("hello").isPresent());
+        assertFalse(findLongIn("vdfsvfbf").isPresent());
     }
 
     private static void aeq(Object a, Object b) {
