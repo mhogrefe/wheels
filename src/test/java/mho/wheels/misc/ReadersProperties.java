14a7c2f12896366012fc972ba6c1d34a9db53637
mhogrefe <mikhailhogrefe@gmail.com>
2014-12-14 23:59:02 -0500

    added more numeric read and findIn properties

--- src/test/java/mho/wheels/misc/ReadersProperties.java
+++ src/test/java/mho/wheels/misc/ReadersProperties.java
@@ -7,6 +7,7 @@ import mho.wheels.ordering.Ordering;
 import mho.wheels.structures.Pair;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Optional;
@@ -44,6 +45,16 @@ public class ReadersProperties {
             propertiesFindOrderingIn();
             propertiesReadRoundingMode();
             propertiesFindRoundingModeIn();
+            propertiesReadBigInteger();
+            propertiesFindBigIntegerIn();
+            propertiesReadByte();
+            propertiesFindByteIn();
+            propertiesReadShort();
+            propertiesFindShortIn();
+            propertiesReadInteger();
+            propertiesFindIntegerIn();
+            propertiesReadLong();
+            propertiesFindLongIn();
         }
         System.out.println("Done");
     }
@@ -204,9 +215,218 @@ public class ReadersProperties {
             );
         }
         for (String s : take(LIMIT, ss)) {
-            System.out.println(s);
             Optional<Pair<RoundingMode, Integer>> op = findRoundingModeIn(s);
             Pair<RoundingMode, Integer> p = op.get();
+            assertNotNull(s, p.a);
+            assertNotNull(s, p.b);
+            assertTrue(s, p.b >= 0 && p.b < s.length());
+            assertTrue(s, s.substring(p.b).startsWith(p.a.toString()));
+        }
+    }
+
+    private static void propertiesReadBigInteger() {
+        initialize();
+        System.out.println("testing readBigInteger(String) properties...");
+
+        for (String s : take(LIMIT, P.strings())) {
+            readBigInteger(s);
+        }
+
+        for (BigInteger i : take(LIMIT, P.bigIntegers())) {
+            Optional<BigInteger> orm = readBigInteger(i.toString());
+            assertEquals(i.toString(), orm.get(), i);
+        }
+    }
+
+    private static void propertiesFindBigIntegerIn() {
+        initialize();
+        System.out.println("testing findBigIntegerIn(String) properties...");
+
+        for (String s : take(LIMIT, P.strings())) {
+            findBigIntegerIn(s);
+        }
+
+        Iterable<Pair<String, Integer>> ps = P.dependentPairsLogarithmic(P.strings(), s -> range(0, s.length()));
+        Iterable<String> ss = map(
+                p -> {
+                    assert p.a != null;
+                    assert p.a.a != null;
+                    assert p.a.b != null;
+                    return take(p.a.b, p.a.a) + p.b + drop(p.a.b, p.a.a);
+                },
+                P.pairs(ps, P.bigIntegers())
+        );
+        for (String s : take(LIMIT, ss)) {
+            Optional<Pair<BigInteger, Integer>> op = findBigIntegerIn(s);
+            Pair<BigInteger, Integer> p = op.get();
+            assertNotNull(s, p.a);
+            assertNotNull(s, p.b);
+            assertTrue(s, p.b >= 0 && p.b < s.length());
+            assertTrue(s, s.substring(p.b).startsWith(p.a.toString()));
+        }
+    }
+
+    private static void propertiesReadByte() {
+        initialize();
+        System.out.println("testing readByte(String) properties...");
+
+        for (String s : take(LIMIT, P.strings())) {
+            readByte(s);
+        }
+
+        for (byte b : take(LIMIT, P.bytes())) {
+            Optional<Byte> ob = readByte(Byte.toString(b));
+            assertEquals(Byte.toString(b), ob.get(), Byte.valueOf(b));
+        }
+    }
+
+    private static void propertiesFindByteIn() {
+        initialize();
+        System.out.println("testing findByteIn(String) properties...");
+
+        for (byte b : take(LIMIT, P.bytes())) {
+            findByteIn(Byte.toString(b));
+        }
+
+        Iterable<Pair<String, Integer>> ps = P.dependentPairsLogarithmic(P.strings(), s -> range(0, s.length()));
+        Iterable<String> ss = map(
+                p -> {
+                    assert p.a != null;
+                    assert p.a.a != null;
+                    assert p.a.b != null;
+                    return take(p.a.b, p.a.a) + p.b + drop(p.a.b, p.a.a);
+                },
+                P.pairs(ps, P.bytes())
+        );
+        for (String s : take(LIMIT, ss)) {
+            Optional<Pair<Byte, Integer>> op = findByteIn(s);
+            Pair<Byte, Integer> p = op.get();
+            assertNotNull(s, p.a);
+            assertNotNull(s, p.b);
+            assertTrue(s, p.b >= 0 && p.b < s.length());
+            assertTrue(s, s.substring(p.b).startsWith(p.a.toString()));
+        }
+    }
+
+    private static void propertiesReadShort() {
+        initialize();
+        System.out.println("testing readShort(String) properties...");
+
+        for (String s : take(LIMIT, P.strings())) {
+            readShort(s);
+        }
+
+        for (short s : take(LIMIT, P.shorts())) {
+            Optional<Short> os = readShort(Short.toString(s));
+            assertEquals(Short.toString(s), os.get(), Short.valueOf(s));
+        }
+    }
+
+    private static void propertiesFindShortIn() {
+        initialize();
+        System.out.println("testing findShortIn(String) properties...");
+
+        for (short s : take(LIMIT, P.shorts())) {
+            findByteIn(Short.toString(s));
+        }
+
+        Iterable<Pair<String, Integer>> ps = P.dependentPairsLogarithmic(P.strings(), s -> range(0, s.length()));
+        Iterable<String> ss = map(
+                p -> {
+                    assert p.a != null;
+                    assert p.a.a != null;
+                    assert p.a.b != null;
+                    return take(p.a.b, p.a.a) + p.b + drop(p.a.b, p.a.a);
+                },
+                P.pairs(ps, P.shorts())
+        );
+        for (String s : take(LIMIT, ss)) {
+            Optional<Pair<Short, Integer>> op = findShortIn(s);
+            Pair<Short, Integer> p = op.get();
+            assertNotNull(s, p.a);
+            assertNotNull(s, p.b);
+            assertTrue(s, p.b >= 0 && p.b < s.length());
+            assertTrue(s, s.substring(p.b).startsWith(p.a.toString()));
+        }
+    }
+
+    private static void propertiesReadInteger() {
+        initialize();
+        System.out.println("testing readInteger(String) properties...");
+
+        for (String s : take(LIMIT, P.strings())) {
+            readInteger(s);
+        }
+
+        for (int i : take(LIMIT, P.integers())) {
+            Optional<Integer> oi = readInteger(Integer.toString(i));
+            assertEquals(Integer.toString(i), oi.get(), Integer.valueOf(i));
+        }
+    }
+
+    private static void propertiesFindIntegerIn() {
+        initialize();
+        System.out.println("testing findIntegerIn(String) properties...");
+
+        for (int i : take(LIMIT, P.integers())) {
+            findByteIn(Integer.toString(i));
+        }
+
+        Iterable<Pair<String, Integer>> ps = P.dependentPairsLogarithmic(P.strings(), s -> range(0, s.length()));
+        Iterable<String> ss = map(
+                p -> {
+                    assert p.a != null;
+                    assert p.a.a != null;
+                    assert p.a.b != null;
+                    return take(p.a.b, p.a.a) + p.b + drop(p.a.b, p.a.a);
+                },
+                P.pairs(ps, P.integers())
+        );
+        for (String s : take(LIMIT, ss)) {
+            Optional<Pair<Integer, Integer>> op = findIntegerIn(s);
+            Pair<Integer, Integer> p = op.get();
+            assertNotNull(s, p.a);
+            assertNotNull(s, p.b);
+            assertTrue(s, p.b >= 0 && p.b < s.length());
+            assertTrue(s, s.substring(p.b).startsWith(p.a.toString()));
+        }
+    }
+
+    private static void propertiesReadLong() {
+        initialize();
+        System.out.println("testing readLong(String) properties...");
+
+        for (String s : take(LIMIT, P.strings())) {
+            readLong(s);
+        }
+
+        for (long l : take(LIMIT, P.longs())) {
+            Optional<Long> ol = readLong(Long.toString(l));
+            assertEquals(Long.toString(l), ol.get(), Long.valueOf(l));
+        }
+    }
+
+    private static void propertiesFindLongIn() {
+        initialize();
+        System.out.println("testing findLongIn(String) properties...");
+
+        for (long l : take(LIMIT, P.bytes())) {
+            findByteIn(Long.toString(l));
+        }
+
+        Iterable<Pair<String, Integer>> ps = P.dependentPairsLogarithmic(P.strings(), s -> range(0, s.length()));
+        Iterable<String> ss = map(
+                p -> {
+                    assert p.a != null;
+                    assert p.a.a != null;
+                    assert p.a.b != null;
+                    return take(p.a.b, p.a.a) + p.b + drop(p.a.b, p.a.a);
+                },
+                P.pairs(ps, P.longs())
+        );
+        for (String s : take(LIMIT, ss)) {
+            Optional<Pair<Long, Integer>> op = findLongIn(s);
+            Pair<Long, Integer> p = op.get();
             assertNotNull(s, p.a);
             assertNotNull(s, p.b);
             assertTrue(s, p.b >= 0 && p.b < s.length());
