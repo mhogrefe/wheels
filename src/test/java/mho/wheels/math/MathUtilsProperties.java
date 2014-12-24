b620496c9aa5991a751888ba78935502e4fa974b
mhogrefe <mikhailhogrefe@gmail.com>
2014-12-23 21:14:28 -0500

    added gcd(long, long) properties and ICTs

--- src/test/java/mho/wheels/math/MathUtilsProperties.java
+++ src/test/java/mho/wheels/math/MathUtilsProperties.java
@@ -36,17 +36,19 @@ public class MathUtilsProperties {
         for (boolean useRandom : Arrays.asList(false, true)) {
             System.out.println("\ttesting " + (useRandom ? "randomly" : "exhaustively"));
             USE_RANDOM = useRandom;
-            propertiesGcd();
-            compareImplementationsGcd();
+            propertiesGcd_int_int();
+            compareImplementationsGcd_int_int();
+            propertiesGcd_long_long();
+            compareImplementationsGcd_long_long();
         }
         System.out.println("Done");
     }
 
-    private static int gcd_simplest(int x, int y) {
+    private static int gcd_int_int_simplest(int x, int y) {
         return BigInteger.valueOf(x).gcd(BigInteger.valueOf(y)).intValue();
     }
 
-    private static int gcd_explicit(int x, int y) {
+    private static int gcd_int_int_explicit(int x, int y) {
         x = Math.abs(x);
         y = Math.abs(y);
         if (x == 0) return y;
@@ -54,7 +56,7 @@ public class MathUtilsProperties {
         return maximum(intersect(factors(x), factors(y)));
     }
 
-    private static void propertiesGcd() {
+    private static void propertiesGcd_int_int() {
         initialize();
         System.out.println("\t\ttesting gcd(int, int) properties...");
 
@@ -63,13 +65,14 @@ public class MathUtilsProperties {
             assert p.a != null;
             assert p.b != null;
             int gcd = gcd(p.a, p.b);
-            assertEquals(p.toString(), gcd, gcd_simplest(p.a, p.b));
-            assertEquals(p.toString(), gcd, gcd_explicit(p.a, p.b));
+            assertEquals(p.toString(), gcd, gcd_int_int_simplest(p.a, p.b));
+            assertEquals(p.toString(), gcd, gcd_int_int_explicit(p.a, p.b));
             assertTrue(p.toString(), gcd >= 0);
+            assertEquals(p.toString(), gcd, gcd(Math.abs(p.a), Math.abs(p.b)));
         }
     }
 
-    private static void compareImplementationsGcd() {
+    private static void compareImplementationsGcd_int_int() {
         initialize();
         System.out.println("\t\tcomparing gcd(int, int) implementations...");
 
@@ -79,7 +82,7 @@ public class MathUtilsProperties {
             long time = System.nanoTime();
             assert p.a != null;
             assert p.b != null;
-            gcd_simplest(p.a, p.b);
+            gcd_int_int_simplest(p.a, p.b);
             totalTime += (System.nanoTime() - time);
         }
         System.out.println("\t\t\tsimplest: " + ((double) totalTime) / 1e9 + " s");
@@ -88,13 +91,82 @@ public class MathUtilsProperties {
             long time = System.nanoTime();
             assert p.a != null;
             assert p.b != null;
-            gcd_explicit(p.a, p.b);
+            gcd_int_int_explicit(p.a, p.b);
             totalTime += (System.nanoTime() - time);
         }
         System.out.println("\t\t\texplicit: " + ((double) totalTime) / 1e9 + " s");
 
         totalTime = 0;
         for (Pair<Integer, Integer> p : take(LIMIT, ps)) {
+            long time = System.nanoTime();
+            assert p.a != null;
+            assert p.b != null;
+            gcd(p.a, p.b);
+            totalTime += (System.nanoTime() - time);
+        }
+        System.out.println("\t\t\tstandard: " + ((double) totalTime) / 1e9 + " s");
+    }
+
+    private static long gcd_long_long_simplest(long x, long y) {
+        return BigInteger.valueOf(x).gcd(BigInteger.valueOf(y)).longValue();
+    }
+
+    private static long gcd_long_long_explicit(long x, long y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        if (x == 0) return y;
+        if (y == 0) return x;
+        return maximum(intersect(factors(BigInteger.valueOf(x)), factors(BigInteger.valueOf(y)))).longValue();
+    }
+
+    private static void propertiesGcd_long_long() {
+        initialize();
+        System.out.println("\t\ttesting gcd(long, long) properties...");
+
+        Iterable<Pair<Long, Long>> ps = filter(p -> p.a != 0 || p.b != 0, P.pairs(P.longs()));
+        for (Pair<Long, Long> p : take(LIMIT, ps)) {
+            assert p.a != null;
+            assert p.b != null;
+            long gcd = gcd(p.a, p.b);
+            assertEquals(p.toString(), gcd, gcd_long_long_simplest(p.a, p.b));
+            if (Math.abs(p.a) <= Integer.MAX_VALUE && Math.abs(p.b) <= Integer.MAX_VALUE) {
+                assertEquals(p.toString(), gcd, gcd_long_long_explicit(p.a, p.b));
+            }
+            assertTrue(p.toString(), gcd >= 0);
+            assertEquals(p.toString(), gcd, gcd(Math.abs(p.a), Math.abs(p.b)));
+        }
+    }
+
+    private static void compareImplementationsGcd_long_long() {
+        initialize();
+        System.out.println("\t\tcomparing gcd(long, long) implementations...");
+
+        long totalTime = 0;
+        Iterable<Pair<Long, Long>> ps = filter(p -> p.a != 0 || p.b != 0, P.pairs(P.longs()));
+        for (Pair<Long, Long> p : take(LIMIT, ps)) {
+            long time = System.nanoTime();
+            assert p.a != null;
+            assert p.b != null;
+            gcd_long_long_simplest(p.a, p.b);
+            totalTime += (System.nanoTime() - time);
+        }
+        System.out.println("\t\t\tsimplest: " + ((double) totalTime) / 1e9 + " s");
+
+        if (P instanceof ExhaustiveProvider) {
+            for (Pair<Long, Long> p : take(LIMIT, ps)) {
+                long time = System.nanoTime();
+                assert p.a != null;
+                assert p.b != null;
+                gcd_long_long_explicit(p.a, p.b);
+                totalTime += (System.nanoTime() - time);
+            }
+            System.out.println("\t\t\texplicit: " + ((double) totalTime) / 1e9 + " s");
+        } else {
+            System.out.println("\t\t\texplicit: too long");
+        }
+
+        totalTime = 0;
+        for (Pair<Long, Long> p : take(LIMIT, ps)) {
             long time = System.nanoTime();
             assert p.a != null;
             assert p.b != null;
