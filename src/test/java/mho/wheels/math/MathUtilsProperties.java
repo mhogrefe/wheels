24a9b1ad84833e95b0fa58fbd23e29053ed1a2d2
mhogrefe <mikhailhogrefe@gmail.com>
2014-12-23 20:59:14 -0500

    added gcd ICTs

--- src/test/java/mho/wheels/math/MathUtilsProperties.java
+++ src/test/java/mho/wheels/math/MathUtilsProperties.java
@@ -37,12 +37,21 @@ public class MathUtilsProperties {
             System.out.println("\ttesting " + (useRandom ? "randomly" : "exhaustively"));
             USE_RANDOM = useRandom;
             propertiesGcd();
+            compareImplementationsGcd();
         }
         System.out.println("Done");
     }
 
     private static int gcd_simplest(int x, int y) {
         return BigInteger.valueOf(x).gcd(BigInteger.valueOf(y)).intValue();
+    }
+
+    private static int gcd_explicit(int x, int y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        if (x == 0) return y;
+        if (y == 0) return x;
+        return maximum(intersect(factors(x), factors(y)));
     }
 
     private static void propertiesGcd() {
@@ -55,7 +64,43 @@ public class MathUtilsProperties {
             assert p.b != null;
             int gcd = gcd(p.a, p.b);
             assertEquals(p.toString(), gcd, gcd_simplest(p.a, p.b));
+            assertEquals(p.toString(), gcd, gcd_explicit(p.a, p.b));
             assertTrue(p.toString(), gcd >= 0);
         }
+    }
+
+    private static void compareImplementationsGcd() {
+        initialize();
+        System.out.println("\t\tcomparing gcd(int, int) implementations...");
+
+        long totalTime = 0;
+        Iterable<Pair<Integer, Integer>> ps = filter(p -> p.a != 0 || p.b != 0, P.pairs(P.integers()));
+        for (Pair<Integer, Integer> p : take(LIMIT, ps)) {
+            long time = System.nanoTime();
+            assert p.a != null;
+            assert p.b != null;
+            gcd_simplest(p.a, p.b);
+            totalTime += (System.nanoTime() - time);
+        }
+        System.out.println("\t\t\tsimplest: " + ((double) totalTime) / 1e9 + " s");
+
+        for (Pair<Integer, Integer> p : take(LIMIT, ps)) {
+            long time = System.nanoTime();
+            assert p.a != null;
+            assert p.b != null;
+            gcd_explicit(p.a, p.b);
+            totalTime += (System.nanoTime() - time);
+        }
+        System.out.println("\t\t\texplicit: " + ((double) totalTime) / 1e9 + " s");
+
+        totalTime = 0;
+        for (Pair<Integer, Integer> p : take(LIMIT, ps)) {
+            long time = System.nanoTime();
+            assert p.a != null;
+            assert p.b != null;
+            gcd(p.a, p.b);
+            totalTime += (System.nanoTime() - time);
+        }
+        System.out.println("\t\t\tstandard: " + ((double) totalTime) / 1e9 + " s");
     }
 }
