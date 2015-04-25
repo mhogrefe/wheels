cdf7a729cc2b9ff71d80c1f3407b82169d58c674
mhogrefe <mikhailhogrefe@gmail.com>
2015-04-24 20:06:21 -0400

    better variable names

--- src/main/java/mho/wheels/random/IsaacPRNG.java
+++ src/main/java/mho/wheels/random/IsaacPRNG.java
@@ -4,32 +4,32 @@ import java.util.Random;
 
 // Implementation by Bob Jenkins
 public class IsaacPRNG {
-    final static int SIZE_BITS = 8;          //log of size of rsl[] and mem[]
-    final static int SIZE = 1 << SIZE_BITS;  // size of rsl[] and mem[]
+    final static int SIZE_BITS = 8;          // log of size of result[] and state[]
+    final static int SIZE = 1 << SIZE_BITS;  // size of result[] and state[]
     final static int MASK = (SIZE - 1) << 2; // for pseudorandom lookup
-    int count;                               // count through the results in rsl[]
-    int rsl[];                               // the results given to the user
-    private int mem[];                       // the internal state
+    int count;                               // count through the results in result[]
+    int result[];                            // the results given to the user
+    private int state[];                     // the internal state
     private int a;                           // accumulator
     private int b;                           // the last result
     private int c;                           // counter, guarantees cycle is at least 2^40
 
     // Seed from system time
     public IsaacPRNG() {
-        mem = new int[SIZE];
-        rsl = new int[SIZE];
+        state = new int[SIZE];
+        result = new int[SIZE];
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
-            rsl[i] = random.nextInt();
+            result[i] = random.nextInt();
         }
         initialize();
     }
 
     // Equivalent to randinit(ctx, TRUE) after putting seed in randctx in C
     public IsaacPRNG(int seed[]) {
-        mem = new int[SIZE];
-        rsl = new int[SIZE];
-        System.arraycopy(seed, 0, rsl, 0, seed.length);
+        state = new int[SIZE];
+        result = new int[SIZE];
+        System.arraycopy(seed, 0, result, 0, seed.length);
         initialize();
     }
 
@@ -40,71 +40,71 @@ public class IsaacPRNG {
         int j = SIZE / 2;
         b += ++c;
         while (i < SIZE / 2) {
-            int x = mem[i];
+            int x = state[i];
             a ^= a << 13;
-            a += mem[j++];
-            int y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            int y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
 
-            x = mem[i];
+            x = state[i];
             a ^= a >>> 6;
-            a += mem[j++];
-            y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
 
-            x = mem[i];
+            x = state[i];
             a ^= a<<2;
-            a += mem[j++];
-            y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
 
-            x = mem[i];
+            x = state[i];
             a ^= a >>> 16;
-            a += mem[j++];
-            y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
         }
 
         j = 0;
         while (j < SIZE / 2) {
-            int x = mem[i];
+            int x = state[i];
             a ^= a << 13;
-            a += mem[j++];
-            int y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            int y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
 
-            x = mem[i];
+            x = state[i];
             a ^= a >>> 6;
-            a += mem[j++];
-            mem[i] = y = mem[(x & MASK) >> 2] + a + b;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            state[i] = y = state[(x & MASK) >> 2] + a + b;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
 
-            x = mem[i];
+            x = state[i];
             a ^= a << 2;
-            a += mem[j++];
-            y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
 
-            x = mem[i];
+            x = state[i];
             a ^= a >>> 16;
-            a += mem[j++];
-            y = mem[(x & MASK) >> 2] + a + b;
-            mem[i] = y;
-            b = mem[((y >> SIZE_BITS) & MASK) >> 2] + x;
-            rsl[i++] = b;
+            a += state[j++];
+            y = state[(x & MASK) >> 2] + a + b;
+            state[i] = y;
+            b = state[((y >> SIZE_BITS) & MASK) >> 2] + x;
+            result[i++] = b;
         }
     }
 
@@ -152,15 +152,15 @@ public class IsaacPRNG {
             a += b;
         }
 
-        for (int i = 0; i < SIZE; i += 8) {              // fill in mem[] with messy stuff
-            a += rsl[i];
-            b += rsl[i + 1];
-            c += rsl[i + 2];
-            d += rsl[i + 3];
-            e += rsl[i + 4];
-            f += rsl[i + 5];
-            g += rsl[i + 6];
-            h += rsl[i + 7];
+        for (int i = 0; i < SIZE; i += 8) {              // fill in state[] with messy stuff
+            a += result[i];
+            b += result[i + 1];
+            c += result[i + 2];
+            d += result[i + 3];
+            e += result[i + 4];
+            f += result[i + 5];
+            g += result[i + 6];
+            h += result[i + 7];
 
             a ^= b << 11;
             d += a;
@@ -194,26 +194,26 @@ public class IsaacPRNG {
             c += h;
             a += b;
 
-            mem[i] = a;
-            mem[i + 1] = b;
-            mem[i + 2] = c;
-            mem[i + 3] = d;
-            mem[i + 4] = e;
-            mem[i + 5] = f;
-            mem[i + 6] = g;
-            mem[i + 7] = h;
+            state[i] = a;
+            state[i + 1] = b;
+            state[i + 2] = c;
+            state[i + 3] = d;
+            state[i + 4] = e;
+            state[i + 5] = f;
+            state[i + 6] = g;
+            state[i + 7] = h;
         }
 
-        // second pass makes all of seed affect all of mem
+        // second pass makes all of seed affect all of state
         for (int i = 0; i < SIZE; i += 8) {
-            a += mem[i];
-            b += mem[i + 1];
-            c += mem[i + 2];
-            d += mem[i + 3];
-            e += mem[i + 4];
-            f += mem[i + 5];
-            g += mem[i + 6];
-            h += mem[i + 7];
+            a += state[i];
+            b += state[i + 1];
+            c += state[i + 2];
+            d += state[i + 3];
+            e += state[i + 4];
+            f += state[i + 5];
+            g += state[i + 6];
+            h += state[i + 7];
 
             a ^= b << 11;
             d += a;
@@ -247,14 +247,14 @@ public class IsaacPRNG {
             c += h;
             a += b;
 
-            mem[i] = a;
-            mem[i + 1] = b;
-            mem[i + 2] = c;
-            mem[i + 3] = d;
-            mem[i + 4] = e;
-            mem[i + 5] = f;
-            mem[i + 6] = g;
-            mem[i + 7] = h;
+            state[i] = a;
+            state[i + 1] = b;
+            state[i + 2] = c;
+            state[i + 3] = d;
+            state[i + 4] = e;
+            state[i + 5] = f;
+            state[i + 6] = g;
+            state[i + 7] = h;
         }
 
         generate();
@@ -266,6 +266,6 @@ public class IsaacPRNG {
             generate();
             count = SIZE - 1;
         }
-        return rsl[count];
+        return result[count];
     }
 }
