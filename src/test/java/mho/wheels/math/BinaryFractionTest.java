127f10e919bbab3a6ae8ed2e3a9195cd786867f0
mhogrefe <mikhailhogrefe@gmail.com>
2015-05-27 21:38:05 -0400

    added getter tests

--- src/test/java/mho/wheels/math/BinaryFractionTest.java
+++ src/test/java/mho/wheels/math/BinaryFractionTest.java
@@ -1,5 +1,6 @@
 package mho.wheels.math;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import static mho.wheels.math.BinaryFraction.*;
@@ -18,5 +19,39 @@ public class BinaryFractionTest {
         aeq(LARGEST_SUBNORMAL_DOUBLE, "4503599627370495 >> 1074");
         aeq(SMALLEST_NORMAL_DOUBLE, "1 >> 1022");
         aeq(LARGEST_DOUBLE, "9007199254740991 << 971");
+    }
+
+    private static void getMantissa_helper(@NotNull String x, int output) {
+        aeq(read(x).get().getMantissa(), output);
+    }
+
+    @Test
+    public void testGetMantissa() {
+        getMantissa_helper("0", 0);
+        getMantissa_helper("1", 1);
+        getMantissa_helper("11", 11);
+        getMantissa_helper("5 << 20", 5);
+        getMantissa_helper("5 >> 20", 5);
+        getMantissa_helper("-1", -1);
+        getMantissa_helper("-11", -11);
+        getMantissa_helper("-5 << 20", -5);
+        getMantissa_helper("-5 >> 20", -5);
+    }
+
+    private static void getExponent_helper(@NotNull String x, int output) {
+        aeq(read(x).get().getExponent(), output);
+    }
+
+    @Test
+    public void testGetExponent() {
+        getExponent_helper("0", 0);
+        getExponent_helper("1", 0);
+        getExponent_helper("11", 0);
+        getExponent_helper("5 << 20", 20);
+        getExponent_helper("5 >> 20", -20);
+        getExponent_helper("-1", 0);
+        getExponent_helper("-11", 0);
+        getExponent_helper("-5 << 20", 20);
+        getExponent_helper("-5 >> 20", -20);
     }
 }
