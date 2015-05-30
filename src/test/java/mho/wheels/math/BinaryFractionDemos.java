65865bbc8231fe9295305446ebd797e6f7c01f84
mhogrefe <mikhailhogrefe@gmail.com>
2015-05-30 14:40:19 -0400

    added factory method demos

--- src/test/java/mho/wheels/math/BinaryFractionDemos.java
+++ src/test/java/mho/wheels/math/BinaryFractionDemos.java
@@ -3,8 +3,12 @@ package mho.wheels.math;
 import mho.wheels.iterables.ExhaustiveProvider;
 import mho.wheels.iterables.IterableProvider;
 import mho.wheels.iterables.RandomProvider;
+import mho.wheels.structures.Pair;
+
+import java.math.BigInteger;
 
 import static mho.wheels.iterables.IterableUtils.take;
+import static mho.wheels.math.BinaryFraction.*;
 
 @SuppressWarnings({"UnusedDeclaration", "ConstantConditions"})
 public class BinaryFractionDemos {
@@ -33,6 +37,41 @@ public class BinaryFractionDemos {
         initialize();
         for (BinaryFraction bf : take(LIMIT, P.binaryFractions())) {
             System.out.println("getExponent(" + bf + ") = " + bf.getExponent());
+        }
+    }
+
+    private static void demoOf_BigInteger_int() {
+        initialize();
+        for (Pair<BigInteger, Integer> p : take(LIMIT, P.pairs(P.bigIntegers(), P.integers()))) {
+            System.out.println("of(" + p.a + ", " + p.b + ") = " + of(p.a, p.b));
+        }
+    }
+
+    private static void demoOf_BigInteger() {
+        initialize();
+        for (BigInteger i : take(LIMIT, P.bigIntegers())) {
+            System.out.println("of(" + i + ") = " + of(i));
+        }
+    }
+
+    private static void demoOf_int() {
+        initialize();
+        for (int i : take(LIMIT, P.integers())) {
+            System.out.println("of(" + i + ") = " + of(i));
+        }
+    }
+
+    private static void demoOf_float() {
+        initialize();
+        for (float f : take(LIMIT, P.floats())) {
+            System.out.println("of(" + f + ") = " + of(f));
+        }
+    }
+
+    private static void demoOf_double() {
+        initialize();
+        for (double d : take(LIMIT, P.doubles())) {
+            System.out.println("of(" + d + ") = " + of(d));
         }
     }
 
