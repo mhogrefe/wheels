e7d73c268a366e2de8b290f92b8b1ff7eac4ea0f
mhogrefe <mikhailhogrefe@gmail.com>
2015-07-21 00:42:24 -0400

    cleaned up BigDecimalUtils props

--- src/test/java/mho/wheels/numberUtils/BigDecimalUtilsProperties.java
+++ src/test/java/mho/wheels/numberUtils/BigDecimalUtilsProperties.java
@@ -22,6 +22,10 @@ public class BigDecimalUtilsProperties {
     private static int LIMIT;
     private static IterableProvider P;
 
+    private static void initialize(String name) {
+        System.out.println("\t\ttesting " + name + " properties...");
+    }
+
     @Test
     public void testAllProperties() {
         List<Triple<IterableProvider, Integer, String>> configs = new ArrayList<>();
@@ -40,8 +44,7 @@ public class BigDecimalUtilsProperties {
     }
 
     private static void propertiesSetPrecision() {
-        System.out.println("\t\ttesting setPrecision(BigDecimal, int) properties...");
-
+        initialize("setPrecision(BigDecimal, int)");
         Iterable<Pair<BigDecimal, Integer>> ps = filterInfinite(
                 q -> ne(q.a, BigDecimal.ZERO),
                 P.pairsSquareRootOrder(P.bigDecimals(), P.positiveIntegersGeometric())
@@ -79,24 +82,23 @@ public class BigDecimalUtilsProperties {
     }
 
     private static void propertiesSuccessor() {
-        System.out.println("\t\ttesting successor(BigDecimal) properties...");
-
+        initialize("successor(BigDecimal)");
         for (BigDecimal bd : take(LIMIT, P.bigDecimals())) {
             BigDecimal successor = successor(bd);
-            assertEquals(bd.toString(), bd.scale(), successor.scale());
-            assertEquals(bd.toString(), predecessor(successor), bd);
-            assertEquals(bd.toString(), successor(bd.negate()), predecessor(bd).negate());
+            assertEquals(bd, bd.scale(), successor.scale());
+            assertEquals(bd, predecessor(successor), bd);
+            assertEquals(bd, successor(bd.negate()), predecessor(bd).negate());
         }
     }
 
     private static void propertiesPredecessor() {
-        System.out.println("\t\ttesting predecessor(BigDecimal) properties...");
+        initialize("predecessor(BigDecimal)");
 
         for (BigDecimal bd : take(LIMIT, P.bigDecimals())) {
             BigDecimal predecessor = predecessor(bd);
-            assertEquals(bd.toString(), bd.scale(), predecessor.scale());
-            assertEquals(bd.toString(), successor(predecessor), bd);
-            assertEquals(bd.toString(), predecessor(bd.negate()), successor(bd).negate());
+            assertEquals(bd, bd.scale(), predecessor.scale());
+            assertEquals(bd, successor(predecessor), bd);
+            assertEquals(bd, predecessor(bd.negate()), successor(bd).negate());
         }
     }
 }
