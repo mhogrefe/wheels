b25dbd2fdf492428ade70a12cc9a548c4740518e
mhogrefe <mikhailhogrefe@gmail.com>
2015-07-28 23:28:31 +0200

    added NoRemoveIterator demos

---
+++ src/test/java/mho/wheels/iterables/NoRemoveIteratorDemos.java
@@ -0,0 +1,69 @@
+package mho.wheels.iterables;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static mho.wheels.iterables.IterableUtils.*;
+import static mho.wheels.testing.Testing.*;
+
+@SuppressWarnings("UnusedDeclaration")
+public class NoRemoveIteratorDemos {
+    private static final boolean USE_RANDOM = false;
+    private static int LIMIT;
+    private static final int SMALL_LIMIT = 1000;
+    private static IterableProvider P;
+
+    private static void initialize() {
+        if (USE_RANDOM) {
+            P = RandomProvider.example();
+            LIMIT = 1000;
+        } else {
+            P = ExhaustiveProvider.INSTANCE;
+            LIMIT = 10000;
+        }
+    }
+
+    private static void demoConstructor_finite() {
+        initialize();
+        for (List<Integer> xs : take(LIMIT, P.lists(P.integers()))) {
+            NoRemoveIterator<Integer> it = new NoRemoveIterator<Integer>() {
+                private int i = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return i < xs.size();
+                }
+
+                @Override
+                public Integer next() {
+                    return xs.get(i++);
+                }
+            };
+            System.out.println("NoRemoveIterable from " + xs + ": " + its(() -> it));
+        }
+    }
+
+    private static void demoConstructor_cyclic() {
+        initialize();
+        Iterable<Iterable<Integer>> xss = map(
+                IterableUtils::cycle,
+                nub(map(IterableUtils::unrepeat, P.listsAtLeast(1, P.integers())))
+        );
+        for (Iterable<Integer> xs : take(SMALL_LIMIT, xss)) {
+            NoRemoveIterator<Integer> it = new NoRemoveIterator<Integer>() {
+                private Iterator<Integer> iterator = xs.iterator();
+
+                @Override
+                public boolean hasNext() {
+                    return true;
+                }
+
+                @Override
+                public Integer next() {
+                    return iterator.next();
+                }
+            };
+            System.out.println("NoRemoveIterable from " + its(xs) + ": " + its(() -> it));
+        }
+    }
+}
