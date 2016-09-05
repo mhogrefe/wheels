97c4f5ee501523eee4c5304c98b46b8b598aa6eb
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2016-09-05 15:04:59 -0400

    added more Ordering demos

--- src/test/java/mho/wheels/ordering/OrderingDemos.java
+++ src/test/java/mho/wheels/ordering/OrderingDemos.java
@@ -35,7 +35,7 @@ public class OrderingDemos extends Demos {
     }
 
     private void demoCompare_T_T() {
-        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integers()))) {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
             System.out.println("compare(" + p.a + ", " + p.b + ") = " + compare(p.a, p.b));
         }
     }
@@ -44,8 +44,8 @@ public class OrderingDemos extends Demos {
         Iterable<Triple<Integer, Integer, Integer>> ts = filter(
                 t -> (t.c == 0) == Objects.equals(t.a, t.b),
                 P.triples(
-                        P.withNull(P.integers()),
-                        P.withNull(P.integers()),
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
                         P.withScale(4).integersGeometric()
                 )
         );
@@ -61,6 +61,186 @@ public class OrderingDemos extends Demos {
             };
             System.out.println("compare(" + t.a + " " + fromInt(t.c) + " " + t.b + ", " + t.a + ", " + t.b + ") = " +
                     compare(comparator, t.a, t.b));
+        }
+    }
+
+    private void demoEq_T_T() {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
+            System.out.println(p.a + " is " + (eq(p.a, p.b) ? "" : "not ") + "equal to " + p.b);
+        }
+    }
+
+    private void demoNe_T_T() {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
+            System.out.println(p.a + " is " + (ne(p.a, p.b) ? "" : "not ") + "unequal to " + p.b);
+        }
+    }
+
+    private void demoLt_T_T() {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
+            System.out.println(p.a + " is " + (lt(p.a, p.b) ? "" : "not ") + "less than " + p.b);
+        }
+    }
+
+    private void demoGt_T_T() {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
+            System.out.println(p.a + " is " + (gt(p.a, p.b) ? "" : "not ") + "greater than " + p.b);
+        }
+    }
+
+    private void demoLe_T_T() {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
+            System.out.println(p.a + " is " + (le(p.a, p.b) ? "" : "not ") + "less than or equal to " + p.b);
+        }
+    }
+
+    private void demoGe_T_T() {
+        for (Pair<Integer, Integer> p : take(LIMIT, P.pairs(P.integersGeometric()))) {
+            System.out.println(p.a + " is " + (ge(p.a, p.b) ? "" : "not ") + "greater than or equal to " + p.b);
+        }
+    }
+
+    private void demoEq_Comparator_T_T() {
+        Iterable<Triple<Integer, Integer, Integer>> ts = filter(
+                t -> (t.c == 0) == Objects.equals(t.a, t.b),
+                P.triples(
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
+                        P.withScale(4).integersGeometric()
+                )
+        );
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, ts)) {
+            Comparator<Integer> comparator = (i, j) -> {
+                if (!Objects.equals(i, t.a)) {
+                    throw new IllegalArgumentException();
+                }
+                if (!Objects.equals(j, t.b)) {
+                    throw new IllegalArgumentException();
+                }
+                return t.c;
+            };
+            System.out.println(t.a + " " + fromInt(t.c) + " " + t.b + ": " + t.a + " is " +
+                    (eq(comparator, t.a, t.b) ? "" : "not ") + "equal to " + t.b);
+        }
+    }
+
+    private void demoNe_Comparator_T_T() {
+        Iterable<Triple<Integer, Integer, Integer>> ts = filter(
+                t -> (t.c == 0) == Objects.equals(t.a, t.b),
+                P.triples(
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
+                        P.withScale(4).integersGeometric()
+                )
+        );
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, ts)) {
+            Comparator<Integer> comparator = (i, j) -> {
+                if (!Objects.equals(i, t.a)) {
+                    throw new IllegalArgumentException();
+                }
+                if (!Objects.equals(j, t.b)) {
+                    throw new IllegalArgumentException();
+                }
+                return t.c;
+            };
+            System.out.println(t.a + " " + fromInt(t.c) + " " + t.b + ": " + t.a + " is " +
+                    (ne(comparator, t.a, t.b) ? "" : "not ") + "unequal to " + t.b);
+        }
+    }
+
+    private void demoLt_Comparator_T_T() {
+        Iterable<Triple<Integer, Integer, Integer>> ts = filter(
+                t -> (t.c == 0) == Objects.equals(t.a, t.b),
+                P.triples(
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
+                        P.withScale(4).integersGeometric()
+                )
+        );
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, ts)) {
+            Comparator<Integer> comparator = (i, j) -> {
+                if (!Objects.equals(i, t.a)) {
+                    throw new IllegalArgumentException();
+                }
+                if (!Objects.equals(j, t.b)) {
+                    throw new IllegalArgumentException();
+                }
+                return t.c;
+            };
+            System.out.println(t.a + " " + fromInt(t.c) + " " + t.b + ": " + t.a + " is " +
+                    (lt(comparator, t.a, t.b) ? "" : "not ") + "less than " + t.b);
+        }
+    }
+
+    private void demoGt_Comparator_T_T() {
+        Iterable<Triple<Integer, Integer, Integer>> ts = filter(
+                t -> (t.c == 0) == Objects.equals(t.a, t.b),
+                P.triples(
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
+                        P.withScale(4).integersGeometric()
+                )
+        );
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, ts)) {
+            Comparator<Integer> comparator = (i, j) -> {
+                if (!Objects.equals(i, t.a)) {
+                    throw new IllegalArgumentException();
+                }
+                if (!Objects.equals(j, t.b)) {
+                    throw new IllegalArgumentException();
+                }
+                return t.c;
+            };
+            System.out.println(t.a + " " + fromInt(t.c) + " " + t.b + ": " + t.a + " is " +
+                    (gt(comparator, t.a, t.b) ? "" : "not ") + "greater than " + t.b);
+        }
+    }
+
+    private void demoLe_Comparator_T_T() {
+        Iterable<Triple<Integer, Integer, Integer>> ts = filter(
+                t -> (t.c == 0) == Objects.equals(t.a, t.b),
+                P.triples(
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
+                        P.withScale(4).integersGeometric()
+                )
+        );
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, ts)) {
+            Comparator<Integer> comparator = (i, j) -> {
+                if (!Objects.equals(i, t.a)) {
+                    throw new IllegalArgumentException();
+                }
+                if (!Objects.equals(j, t.b)) {
+                    throw new IllegalArgumentException();
+                }
+                return t.c;
+            };
+            System.out.println(t.a + " " + fromInt(t.c) + " " + t.b + ": " + t.a + " is " +
+                    (le(comparator, t.a, t.b) ? "" : "not ") + "less than or equal to " + t.b);
+        }
+    }
+
+    private void demoGe_Comparator_T_T() {
+        Iterable<Triple<Integer, Integer, Integer>> ts = filter(
+                t -> (t.c == 0) == Objects.equals(t.a, t.b),
+                P.triples(
+                        P.withNull(P.integersGeometric()),
+                        P.withNull(P.integersGeometric()),
+                        P.withScale(4).integersGeometric()
+                )
+        );
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, ts)) {
+            Comparator<Integer> comparator = (i, j) -> {
+                if (!Objects.equals(i, t.a)) {
+                    throw new IllegalArgumentException();
+                }
+                if (!Objects.equals(j, t.b)) {
+                    throw new IllegalArgumentException();
+                }
+                return t.c;
+            };
+            System.out.println(t.a + " " + fromInt(t.c) + " " + t.b + ": " + t.a + " is " +
+                    (ge(comparator, t.a, t.b) ? "" : "not ") + "greater than or equal to " + t.b);
         }
     }
 
