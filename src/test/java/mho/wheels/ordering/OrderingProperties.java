e9b23d4c6b4e1b93989726d5c320d140d3007686
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2016-09-12 18:52:52 -0400

    continued Ordering props

--- src/test/java/mho/wheels/ordering/OrderingProperties.java
+++ src/test/java/mho/wheels/ordering/OrderingProperties.java
@@ -1,7 +1,11 @@
 package mho.wheels.ordering;
 
+import mho.wheels.iterables.IterableProvider;
+import mho.wheels.structures.Pair;
+import mho.wheels.structures.Triple;
 import mho.wheels.testing.TestProperties;
 
+import static mho.wheels.iterables.IterableUtils.*;
 import static mho.wheels.iterables.IterableUtils.take;
 import static mho.wheels.ordering.Ordering.*;
 import static mho.wheels.testing.Testing.*;
@@ -16,6 +20,7 @@ public class OrderingProperties extends TestProperties {
         propertiesFromInt();
         propertiesToInt();
         propertiesInvert();
+        propertiesCompare_T_T();
     }
 
     private void propertiesFromInt() {
@@ -40,6 +45,31 @@ public class OrderingProperties extends TestProperties {
         for (Ordering o : take(LIMIT, P.orderings())) {
             o.invert();
             involution(Ordering::invert, o);
+        }
+    }
+
+    private void propertiesCompare_T_T() {
+        initialize("compare(T, T)");
+        IterableProvider Q = P.deepCopy();
+        IterableProvider R = P.deepCopy();
+        for (Pair<Integer, Integer> p : take(LIMIT, zip(P.integers(), Q.integers()))) {
+            assertEquals(p, compare(p.a, p.b), EQ);
+        }
+
+        P.reset();
+        Q.reset();
+        for (Pair<Integer, Integer> p : take(LIMIT, EP.pairs(P.integers(), Q.integers()))) {
+            Ordering o = compare(p.a, p.b);
+            assertEquals(p, compare(p.b, p.a), o.invert());
+        }
+
+        P.reset();
+        Q.reset();
+        R.reset();
+        for (Triple<Integer, Integer, Integer> t : take(LIMIT, EP.triples(P.integers(), Q.integers(), R.integers()))) {
+            if (compare(t.a, t.b) == LT && compare(t.b, t.c) == LT) {
+                assertEquals(t, compare(t.a, t.c), LT);
+            }
         }
     }
 }
